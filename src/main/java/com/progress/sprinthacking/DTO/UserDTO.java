0df8a89853c5976d1317698ce40015113e6c99ac
maharjanprogress <progressmaharjan1@gmail.com>
2025-06-25 21:55:13 +0545

    user authentication complete

---
+++ src/main/java/com/progress/sprinthacking/DTO/UserDTO.java
@@ -0,0 +1,10 @@
+package com.progress.sprinthacking.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String userName;
+    private String password;
+    private Long roleId;
+}
